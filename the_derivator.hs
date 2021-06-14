d7012b4042b05ed028064501fd6f2f62f233182e
mpicek <martinpicek4@gmail.com>
2021-06-14 22:52:11 +0200

    Delete Var, add new functions

--- the_derivator.hs
+++ the_derivator.hs
@@ -5,13 +5,18 @@
 -- TODO: Pri vypisovani derivace asi evaluovat vse, co jde (napr n - 1) bude
 --       typicky treba (3 - 1), tak tam proste dat 2
 -- TODO udelat nezavisly na Double
+-- TODO chain rule
 
 data FunctionOP = Const Double
-                | Var
                 | Sum FunctionOP FunctionOP
                 | Mul FunctionOP FunctionOP
                 | Poly FunctionOP -- for example x^3 .. no Muliplier
-                -- deriving (Show)
+
+                | Exp FunctionOP
+                | ExpCustom FunctionOP FunctionOP
+                | Recip FunctionOP
+                | Ln FunctionOP
+                | Log FunctionOP FunctionOP
 
 class Differentiable a where
     derivative :: a -> a
@@ -49,10 +54,8 @@ cleanExpr (Mul x (Const 1)) = x                     -- x*1 = x
 cleanExpr (Poly (Const 0)) = Const 1                -- x^0 = 0
 
 
--- TODO: tackle the problem of equality of Var and Poly 1
 instance Differentiable FunctionOP where
     derivative (Const _)  = Const 0 -- c' = 0
-    derivative Var        = Const 1 -- x' = 1
 
     -- (f + g)' = f' + g'
     -- derivative (Sum (Const x) (Const y)) = 
@@ -62,20 +65,18 @@ instance Differentiable FunctionOP where
     derivative (Mul x y) = Sum (Mul (derivative x) y) (Mul x (derivative y))
 
     -- derivative (Poly c 0) = Poly (Mul c n) (Sum n (Const (-1)))
-    derivative (Poly (Const 0)) = Const 0  -- ~ Const 1
-    derivative (Poly (Const 1)) = Const 1  -- ~ Var
+    derivative (Poly (Const 0)) = Const 0
+    derivative (Poly (Const 1)) = Const 1
     derivative (Poly n) = Mul n (Poly (Sum n (Const (-1))))
 
 instance Evaluate FunctionOP where
     eval _ (Const c)  = c
-    eval x Var        = x
     eval x (Mul y z)  = eval x y * eval x z
     eval x (Sum y z)  = eval x y + eval x z
     eval x (Poly n)   = x ** eval x n
 
 instance Show FunctionOP where
     show (Const x)  = show x
-    show Var        = "x"
 
     -- show (Sum (Const 0) (Const 0)) = ""
     -- show (Sum (Const 0) x) = show x
@@ -91,6 +92,7 @@ instance Show FunctionOP where
     show (Mul x (Poly y)) = show x ++ "*" ++ show (Poly y)
     show (Mul x y) = show x ++ " * " ++ show y
 
+    show (Poly (Const 1)) = "x"
     show (Poly (Const n)) = "x^" ++ show n
     show (Poly n)   = "x ^ (" ++ show n ++ ")"
 
