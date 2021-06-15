9bad627a57938e30e0edf58519cc77b8ae4f8beb
mpicek <martinpicek4@gmail.com>
2021-06-15 08:37:42 +0200

    Add chain rule, add back Var (but renamed to X), Poly is (whatever)^n, not only X^n

--- the_derivator.hs
+++ the_derivator.hs
@@ -7,14 +7,17 @@
 -- TODO udelat nezavisly na Double
 -- TODO chain rule
 
+import Debug.Trace
+debug = flip trace
+
 data FunctionOP = Const Double
+                | X
                 | Sum FunctionOP FunctionOP
                 | Mul FunctionOP FunctionOP
-                | Poly FunctionOP -- for example x^3 .. no Muliplier
+                | Poly FunctionOP FunctionOP -- for example (?)^3
 
                 | Exp FunctionOP
                 | ExpCustom FunctionOP FunctionOP
-                | Recip FunctionOP
                 | Ln FunctionOP
                 | Log FunctionOP FunctionOP
 
@@ -31,7 +34,7 @@ instance Num FunctionOP where
     a - (Const b)         = Sum a (Const (-b))
     a - b                 = Sum a (Mul (Const (-1)) b)
     (Const a) * (Const b) = Const (a * b)
-    a * b                 = Sum a b
+    a * b                 = Mul a b
     negate (Const a)      = Const (-a)
     negate a              = Mul (Const (-1)) a
     abs (Const a)         = if a >= 0 then Const a else Const (-a)
@@ -40,9 +43,10 @@ instance Num FunctionOP where
     signum _              = error "TO BE DONE"
     fromInteger a         = Const (fromIntegral a) -- JESTE SI PROJIT
 
-    
+
 -- cleanExpr expr: cleans the expression = makes it more readable
 cleanExpr :: FunctionOP -> FunctionOP
+cleanExpr X = X
 cleanExpr (Sum (Const 0) (Const 0)) = Const 0       -- 0 + 0 = 0
 cleanExpr (Sum (Const 0) x) = x                     -- 0 + x = x
 cleanExpr (Sum x (Const 0)) = x                     -- x + 0 = x
@@ -51,31 +55,47 @@ cleanExpr (Mul (Const 0) _) = Const 0               -- 0*_ = 0
 cleanExpr (Mul _ (Const 0)) = Const 0               -- _*0 = 0
 cleanExpr (Mul (Const 1) x) = x                     -- 1*x = x
 cleanExpr (Mul x (Const 1)) = x                     -- x*1 = x
-cleanExpr (Poly (Const 0)) = Const 1                -- x^0 = 0
+cleanExpr (Mul (Const a) (Const b)) = Const (a * b)
+cleanExpr (Poly _ (Const 0)) = Const 1              -- x^0 = 0
+cleanExpr x = x
 
 
 instance Differentiable FunctionOP where
+    derivative X = Const 1
     derivative (Const _)  = Const 0 -- c' = 0
 
     -- (f + g)' = f' + g'
     -- derivative (Sum (Const x) (Const y)) = 
-    derivative (Sum x y)  = Sum (derivative x) (derivative y)
+    derivative (Sum x y)  = derivative x + derivative y
+
+
+    derivative (Mul (Const x) y) = Mul (Const x) (derivative y)
+    derivative (Mul x (Const y)) = Mul (Const y) (derivative x)
 
     -- (fg)' = f'g + fg'
-    derivative (Mul x y) = Sum (Mul (derivative x) y) (Mul x (derivative y))
+    derivative (Mul x y) = (derivative x * y) + (x * derivative y)
 
     -- derivative (Poly c 0) = Poly (Mul c n) (Sum n (Const (-1)))
-    derivative (Poly (Const 0)) = Const 0
-    derivative (Poly (Const 1)) = Const 1
-    derivative (Poly n) = Mul n (Poly (Sum n (Const (-1))))
+    derivative (Poly x (Const 0)) = Mul (Const 0) (derivative x) -- chain rule
+    derivative (Poly x (Const 1)) = Mul (Const 1) (derivative x) -- chain rule
+    derivative (Poly x n) = Mul (Mul n (Poly x (Sum n (Const (-1))))) (derivative x) -- chain rule
+
+    derivative (Exp x) = Mul (Exp x) (derivative x) -- chain rule
+
+    -- TODO chain rule
+    derivative (ExpCustom a x) = Mul (ExpCustom a x) (Ln a) -- (5^x)' = 5^x*ln5
+    -- derivative Ln x = Mult (
+
 
 instance Evaluate FunctionOP where
+    eval x X = x
     eval _ (Const c)  = c
     eval x (Mul y z)  = eval x y * eval x z
     eval x (Sum y z)  = eval x y + eval x z
-    eval x (Poly n)   = x ** eval x n
+    eval x (Poly a n)   = eval x a ** eval x n
 
 instance Show FunctionOP where
+    show X = "x"
     show (Const x)  = show x
 
     -- show (Sum (Const 0) (Const 0)) = ""
@@ -83,17 +103,17 @@ instance Show FunctionOP where
     -- show (Sum x (Const 0)) = show x
 
     -- show (Sum (Const x) (Const y)) = show (x + y)
-    show (Sum x y)  = show x ++ " + " ++ show y
+    show (Sum x y)  = "(" ++ show x ++ " + " ++ show y ++ ")"
 
     -- show (Mul (Const 0) _) = ""
     -- show (Mul _ (Const 0)) = ""
     -- show (Mul (Const 1) x) = show x
     -- show (Mul x (Const 1)) = show x
-    show (Mul x (Poly y)) = show x ++ "*" ++ show (Poly y)
+    show (Mul x (Poly a y)) = show x ++ " * " ++ show (Poly a y)
     show (Mul x y) = show x ++ " * " ++ show y
 
-    show (Poly (Const 1)) = "x"
-    show (Poly (Const n)) = "x^" ++ show n
-    show (Poly n)   = "x ^ (" ++ show n ++ ")"
+    show (Poly a (Const 1)) = show a
+    show (Poly a (Const n)) = "(" ++ show a ++ ")" ++ "^" ++ show n
+    show (Poly a n)   = "(" ++ show a ++ ")^(" ++ show n ++ ")"
 
 
